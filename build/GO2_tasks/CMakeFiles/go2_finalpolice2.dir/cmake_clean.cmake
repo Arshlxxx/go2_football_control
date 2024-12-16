369ae9c1f8d6266153f87534db575f71ea4088ba
Mingtaorwth <3145024438@qq.com>
2024-12-16 15:54:40 +0800

    Added contents of src/GO2_tasks as a normal directory

---
+++ build/GO2_tasks/CMakeFiles/go2_finalpolice2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/nvidia/go2_ws/devel/lib/GO2_tasks/go2_finalpolice2"
+  "/home/nvidia/go2_ws/devel/lib/GO2_tasks/go2_finalpolice2.pdb"
+  "CMakeFiles/go2_finalpolice2.dir/example/go2/go2_finalpolice2.cpp.o"
+  "CMakeFiles/go2_finalpolice2.dir/example/go2/go2_finalpolice2.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/go2_finalpolice2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
